91c924bd09d9d01926a2c8936edaa24843117df7
Vinicius Massuchetto <viniciusmassuchetto@gmail.com>
2012-09-26 10:36:28 -0300

    Ops 2.

--- code/plugin-ext-form.php
+++ code/plugin-ext-form.php
@@ -1,14 +1,14 @@
 <form>
 <?php foreach ( $field as $f ) : ?>
     <?php $f = apply_filters( 'display_field', $f ); ?>
-    <?php do_action( 'before_display_field', $field ); ?>
+    <?php do_action( 'before_display_field', $f ); ?>
 
     <input
         name="<?php echo $f['name']; ?>"
         type="<?php echo $f['type']; ?>"
         value="<?php echo $f['value']; ?>" />
 
-    <?php do_action( 'after_display_field', $field ); ?>
+    <?php do_action( 'after_display_field', $f ); ?>
 <?php endforeach; ?>
 
 <?php do_action( 'before_submit' ); ?>
